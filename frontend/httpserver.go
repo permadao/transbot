8c43f7f458a60ec529973df6caf56eb6bbbdf587
weibogao <45678072+cryptowizard0@users.noreply.github.com>
2023-05-15 11:12:57 +0800

    add CORS

--- frontend/httpserver.go
+++ frontend/httpserver.go
@@ -5,6 +5,22 @@ import (
 	"net/http"
 )
 
+func CORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+		// 如果是 OPTIONS 请求，则直接返回
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	// 静态文件的目录
 	fs := http.FileServer(http.Dir("./"))
@@ -14,6 +30,6 @@ func main() {
 
 	port := "8081"
 
-	fmt.Println("Server is running on http://localhost:" + port)
-	http.ListenAndServe(":"+port, nil)
+	fmt.Println("Server is running on http://0.0.0.0:" + port)
+	http.ListenAndServe("0.0.0.0:"+port, CORS(http.DefaultServeMux))
 }
--- service/serve.go
+++ service/serve.go
@@ -16,6 +16,12 @@ func StartServe() {
 	router.Use(gin.Recovery())
 	router.Use(func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
 		c.Next()
 	})
 
